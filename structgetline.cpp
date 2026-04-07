c1c75c36d7e65eec598c8d935cca3046c6361f74
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-07 15:57:04 +0700

    input dan get line

--- structgetline.cpp
+++ structgetline.cpp
@@ -11,3 +11,19 @@ struct orang {
     detailAlamat alamat;
     int umur;
 };
+
+int main() {
+    orang mhs;
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
+
+    cout << "Alamat Desa : ";
+    getline(cin, mhs.alamat.desa);
+
+    cout << "Alamat Kota : ";
+    getline(cin, mhs.alamat.kota);
+
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+}
