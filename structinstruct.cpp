8f88358c206137ace9d66738966cc9846000b0b6
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-07 16:01:34 +0700

    input output

--- structinstruct.cpp
+++ structinstruct.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
 using namespace std;
+
+struct DetailAlamat{
+    string desa;
+    string kota;
+};
+
+struct orang{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+int main()
+{
+    //Deklarasi object dari struct orang
+    orang mhs;
+    //isi object
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : " << endl;
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota : " << endl;
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    //Meampilkan Isi Object
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
