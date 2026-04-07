fe4b6fd8183e7f3fe3609fa9841113df562e5f58
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-07 15:54:49 +0700

    output

--- struct.cpp
+++ struct.cpp
@@ -16,4 +16,9 @@ int main(){
     cout << "Umur:";
     cin  >> mhs.umur;
     cout << endl;
+    
+    
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
