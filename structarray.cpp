dbbc2ae10344ab9c6f0177e7cf4c570dad4b4d2b
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-07 16:00:06 +0700

    input output

--- structarray.cpp
+++ structarray.cpp
@@ -16,5 +16,27 @@ int main() {
 
     for  (int i = 0; i <= 2; i++){
     }
+    orang mhs;
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
 
+    cout << "Alamat Desa : ";
+    getline(cin, mhs.alamat.desa);
+
+    cout << "Alamat Kota : ";
+    getline(cin, mhs.alamat.kota);
+
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    cin.ignore();
+
+    for (int i = 0; i <= 2; i++)
+
+    {
+        cout << "Nama : " << mhs.nama << endl;
+        cout << "Desa : " << mhs.alamat.desa << endl;
+        cout << "Kota : " << mhs.alamat.kota << endl;
+        cout << "Umur : " << mhs.umur << endl;
+    }
 }
