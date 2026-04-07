78d6b839d7c8a94f835a06669b131b7fbcae2f56
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-07 15:51:17 +0700

    cara menulis atau mengisi array

--- array.cpp
+++ array.cpp
@@ -13,4 +13,7 @@ int main(){
     cout << "data ke -3 :" << dataku[2]<<endl;
     //operasi matematika 
     x = dataku[2]*3;
+    //cara menulis atau mengisi array
+    dataku[2]= 600;
+    cout << "data ke -3 yang baru : " << dataku[2]<<endl;
 }
